0d65d1758fb47b220cde1c444001ad2ddea76f79
cpiontko <cynthia.piontkowski@ucsf.edu>
2016-07-21 15:26:58 -0700

    Metrics, Activities, Mentor badge
    moved badge script from Framework to Search, function call to
    SearchPerson & SearchEverything

--- Website/SourceCode/Profiles/Profiles/Framework/JavaScript/UCSF.js
+++ Website/SourceCode/Profiles/Profiles/Framework/JavaScript/UCSF.js
@@ -98,36 +98,3 @@
   }
 });
 
- function buildGadgetAds(linksCount) {
-    //Gadget ads  
-    var mentor = "<li><div class='badge'>"
-            + "<h2 style='margin-bottom:5px'>Passionate about Mentoring?</h2>"
-            + "<p><a href='" + _rootDomain + "/login/default.aspx?method=login&amp;edit=true'>Let others know. Add to your UCSF Profile.</a></p>"
-	    + "<img src='" + _rootDomain +"/profile/Modules/CustomViewPersonGeneralInfo/PhotoHandler.ashx?NodeID=367209' alt='Mitch Feldman' width='62' style='position:absolute;clip:rect(3px,60px,68px,0px);' />"
-            + "<p style='padding-left:66px;'><strong><a href='" + _rootDomain + "/mitchell.feldman'>Mitch Feldman</a></strong><br /> is a Faculty Mentor!</p>"
-            + "</div></li>";
-    var videointro = "<li><a href='https://www.youtube.com/watch?v=YCOA2GWyplY' target='_blank'>"
-            + "<div class='badge'><p style='padding-left:3px'>"
-            + "<img src='" + _rootDomain + "/framework/images/video-ad.png' /></p>"
-            + "<p><strong>Watch UCSF Profiles video introduction!</strong></p></div></a></li>";
-    // for search form pages
-    if ($('.nonavbar').length && !$('#FSSiteDescription').length) {
-        var badge = "<ul id='badge'>" + mentor + videointro + "</ul>";
-        $(badge).insertAfter('.profilesContentPassive');
-        var login = $('#signinlink').attr('href');
-        $('.chatterlink').attr('href', login);
-        $("#badge li").hide();
-        randomtip();
-    }
-    if ($('.mainmenu li').last().text() == 'Sign out') {
-        $("#badge").hide();
-    }
-
-}
-
-
-        this.randomtip = function () {
-            var length = $("#badge li").length;
-            var ran = Math.floor(Math.random() * length) + 1;
-            $("#badge li:nth-child(" + ran + ")").show();
-        };

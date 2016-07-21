1965452b5b5c65d9f1c629898ae6ac500899d40f
cpiontko <cynthia.piontkowski@ucsf.edu>
2016-07-21 15:26:58 -0700

    Metrics, Activities, Mentor badge
    moved badge script from Framework to Search, function call to
    SearchPerson & SearchEverything
    
    Conflicts:
    	Website/SourceCode/Profiles/Profiles/Framework/JavaScript/UCSF.js
    	Website/SourceCode/Profiles/Profiles/Search/JavaScript/UCSF.js

--- Website/SourceCode/Profiles/Profiles/History/Modules/HistoryActivity/HistoryActivity.ascx
+++ Website/SourceCode/Profiles/Profiles/History/Modules/HistoryActivity/HistoryActivity.ascx
@@ -1,18 +1,19 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="HistoryActivity.ascx.cs"
     Inherits="Profiles.History.Modules.HistoryActivity.HistoryActivity" %>
 <div class="activities">
+    <div class="act-heading"><strong>Live Updates</strong></div>
 <asp:Repeater runat="server" ID="rptHistoryActivity" OnItemDataBound="rptHistoryActivity_OnItemDataBound">
     <ItemTemplate>
         <div class="divider"/>
         <div class="act">
-            <div class="act-image"><asp:HyperLink runat="server" ID="linkThumbnail"></asp:HyperLink></div>
-       			<div class="act-body">
-                   <div class="act-userdate">
-    			    <div class="act-user"><asp:HyperLink runat="server" ID="linkProfileURL"></asp:HyperLink></div>
-    			    <div class="date"><asp:Literal runat="server" ID="litDate"></asp:Literal></div>
-    			    <div class="act-msg"><asp:Literal runat="server" ID="litMessage"></asp:Literal></div>
-    		    </div>
-            </div>
+       	   <div class="act-body">
+	       <div class="act-image"><asp:HyperLink runat="server" ID="linkThumbnail"></asp:HyperLink></div>
+               <div class="act-userdate">
+		   <div class="act-user"><asp:HyperLink runat="server" ID="linkProfileURL"></asp:HyperLink></div>
+		   <div class="date"><asp:Literal runat="server" ID="litDate"></asp:Literal></div>
+		</div>
+	    </div>
+    	    <div class="act-msg"><asp:Literal runat="server" ID="litMessage"></asp:Literal></div>
         </div>
     </ItemTemplate>
 </asp:Repeater>
@@ -20,4 +21,4 @@
 <asp:Label ID="Label4" runat="server" />
 <asp:HyperLink ID="linkPrev" runat="server">Previous Page</asp:HyperLink>
 <asp:HyperLink ID="linkNext" runat="server">Next Page</asp:HyperLink>
-<asp:HyperLink ID="linkSeeMore" runat="server" NavigateUrl="~/History/ActivityDetails.aspx">See more Activities</asp:HyperLink>
+<asp:HyperLink ID="linkSeeMore" runat="server" NavigateUrl="~/History/ActivityDetails.aspx"><img src="Images/icon_squareArrow.gif" /> See more Activities</asp:HyperLink>
--- Website/SourceCode/Profiles/Profiles/Search/JavaScript/UCSF.js
+++ Website/SourceCode/Profiles/Profiles/Search/JavaScript/UCSF.js
@@ -43,3 +43,55 @@
   $("#tblSearchResults th:contains('Researcher Type')").css("text-align","left");
   
 });
+
+  
+function UrlExists(url)
+{
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
+}
+
+function buildGadgetAds() {
+    //Gadget ads buildGadgetAds(linksCount) 
+	var mentor = "<li><div class='badge'>"
+		+ "<h2 style='margin-bottom:5px'>Passionate about Mentoring?</h2>"
+		+ "<p><a href='" + _rootDomain + "/login/default.aspx?method=login&amp;edit=true'>Let others know. Add to your UCSF Profile.</a></p>";
+	var mentorpage = _rootDomain + "/mitchell.feldman";
+	var mentorphoto = _rootDomain + "/profile/Modules/CustomViewPersonGeneralInfo/PhotoHandler.ashx?NodeID=367209";
+	var mentorimage = "";
+	if (UrlExists(mentorphoto)) {
+		mentorimage =  "<img src='" + mentorphoto 
+			+ "' alt='Mitch Feldman' width='62' style='float:left;padding-right:5px;' />";
+	}
+	if (UrlExists(mentorpage)) {
+		mentor = mentor + "<p style='height:62px;overflow-y:hidden;'>" + mentorimage
+			+ "<strong><a href='" + mentorpage 
+			+ "'>Mitch Feldman</a></strong><br /> is a Faculty Mentor!</p></div></li>";
+	} else {
+		mentor = mentor + "</div></li>";
+	}
+    var videointro = "<li><a href='https://www.youtube.com/watch?v=YCOA2GWyplY' target='_blank'>"
+            + "<div class='badge'><p style='padding-left:3px'>"
+            + "<img src='" + _rootDomain + "/framework/images/video-ad.png' /></p>"
+            + "<p><strong>Watch UCSF Profiles video introduction!</strong></p></div></a></li>";
+    // for search form pages
+    if ($('.nonavbar').length && !$('#FSSiteDescription').length) {
+        var badge = "<ul id='badge'>" + mentor + videointro + "</ul>";
+        $(badge).insertAfter('.profilesContentPassive');
+        var login = $('#signinlink').attr('href');
+        $("#badge li").hide();
+        randomtip();
+    }
+    if ($('.mainmenu li').last().text() == 'Sign out') {
+        $("#badge").hide();
+    }
+
+}
+
+        this.randomtip = function () {
+            var length = $("#badge li").length;
+            var ran = Math.floor(Math.random() * length) + 1;
+            $("#badge li:nth-child(" + ran + ")").show();
+        };

c348c4935e354b0e9d3f4977e5802fe7db18bf69
mgruzman <moisey.gruzman@ucsf.edu>
2016-10-01 21:47:26 -0700

    fixed issues with authors without listed publications,added ability to run one UCSF user

--- Database/ORNG/Symplectic/UCLibrary_Pubs.ps1
+++ Database/ORNG/Symplectic/UCLibrary_Pubs.ps1
@@ -46,27 +46,31 @@
         }
     }
     $authors=$authors  -replace "'","''"
+    $displayname=$displayname -replace "'","''"
     $title=$doc.feed.entry.title
     $title=$title  -replace "'","''"
-    #write-host "userName="$userName
-    #write-host "lib_userid="$authorid
-    #write-host "lib_pubid="$lib_pubid       
-    #write-host "authors="$authors
-    #write-host "title=" $title
-    #write-host "PMID="$PMID
-    #write-host "PCMID="$PCMID
-    
+    if ($DEBUG -eq 1) {
+        write-host "userName="$userName
+        write-host "lib_userid="$authorid
+        write-host "lib_pubid="$lib_pubid       
+        write-host "authors="$authors
+        write-host "title=" $title
+        write-host "PMID="$PMID
+        write-host "PCMID="$PCMID
+    }
     if (-not $PMID) {$col_PMID="NULL"} else {$col_PMID=$PMID}
     if (-not $PCMID) {$col_PCMID="NULL"} else {$col_PCMID="'"+$PCMID+"'"}
     if (-not $authors) {$col_authors="NULL"} else {$col_authors="'"+$authors+"'"}
     if (-not $title) {$col_title="NULL"} else {$col_title="'"+$title+"'"}
-    #write-host "col_PMID="$col_PMID
-    #write-host "col_PCMID="$col_PCMID
-    #write-host "col_authors="$col_authors
-    #write-host "col_title="$col_title
+    if ($DEBUG -eq 1) {
+        write-host "col_PMID="$col_PMID
+        write-host "col_PCMID="$col_PCMID
+        write-host "col_authors="$col_authors
+        write-host "col_title="$col_title
+    }
     $cmd="insert into [profiles_ucsf].[Symplectic.Elements].[Publication]"+
          " values ('$userName','$displayname',$authorid,$lib_pubid,$col_PMID,$col_PCMID,$col_authors,$col_title)"
-    #write-host "!!!$cmd!!!"
+    if ($DEBUG -eq 1) {write-host "!!!$cmd!!!"}
     SQLExecuteCommand  $sqlConnection $cmd
 }
 
@@ -77,14 +81,15 @@ function GetUserPublications($doc){
       while ($true){
         $lib_pubid=$doc.feed.entry[$i].relationship.related.object.id
         if ("$lib_pubid" -eq ""){break}
-        #    write-host $lib_pubid
+        if ($DEBUG -eq 1) {    write-host $lib_pubid}
         $pubs=$pubs+($lib_pubid  -join ",")
         $i++
       }
     } catch {
         $ErrorMessage = $_.Exception.Message
-        get-date -f MM/dd/yyyy_HH:mm:ss
-        write-host $ErrorMessage
+        $hh=get-date -f MM/dd/yyyy_HH:mm:ss
+        write-host `n$hh   $ErrorMessage
+	    $pubs="Exception"
     }
     return $pubs
 }
@@ -103,9 +108,9 @@ function ReadXML($url){
     	return $xmlstr
     } catch {
         $ErrorMessage = $_.Exception.Message
-        get-date -f MM/dd/yyyy_HH:mm:ss
+        $hh=get-date -f MM/dd/yyyy_HH:mm:ss
         write-host `n"URL"$url
-        write-host `n$ErrorMessage
+        write-host `n$hh   $ErrorMessage
     }
     return $result
 }
@@ -118,6 +123,12 @@ function ReadUserNames ([Data.SqlClient.SqlConnection] $sqlConnection) {
     $sqlCommand.CommandText = "SET NOCOUNT ON; " +
         "Select username,personid from [User.Account].[User] "+
 	"where isActive=1 and personid is not NULL and username is not NULL"
+    if ($users4db -ne "") {
+        $sqlCommand.CommandText=$sqlCommand.CommandText+" and username in "+$users4db+";"
+    } else {
+        $sqlCommand.CommandText=$sqlCommand.CommandText+";"
+    }
+    $sqlcommand.CommandTimeout=120
     $reader = $sqlCommand.ExecuteReader()
     $lines=0
     while ($reader.Read())
@@ -125,64 +136,157 @@ function ReadUserNames ([Data.SqlClient.SqlConnection] $sqlConnection) {
         $userNames=$userNames+($reader[“userName”]  -join ",")
         $personids=$personids+($reader["personid"] -join ",")
         $lines=$lines+1
-        #if ($lines -ge 10 ) {break}      
+        if ($DEBUG -eq 1) {if ($lines -ge 100) {break}}      
     }
+    if ($DEBUG -eq 1) {write-host "all 5 users" $usernames}
     return $userNames 
 }
 
-function InsertXML ([Data.SqlClient.SqlConnection] $sqlConnection,$key,$value) {
- 
+function InsertUserXML ([Data.SqlClient.SqlConnection] $sqlConnection,$key,$value) {
+    if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
+        $sqlConnection.Open()
+    }
     $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
     $sqlCommand.Connection = $sqlConnection
     $sqlCommand.CommandText = "SET NOCOUNT ON; " +
-        " INSERT INTO [Symplectic.Elements].[AllXML] (XmlCol,idtype,id )"+
+        " INSERT INTO [Symplectic.Elements].[UserXML] (XmlCol,idtype,id )"+
         " SELECT CONVERT(XML, BulkColumn) AS BulkColumn,@idtype,@id "+ 
         " FROM OPENROWSET(BULK 'D:\app\DesignSymplistic\data_utf8.txt', SINGLE_BLOB) AS x;"
+    $sqlcommand.CommandTimeout=120
     $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@idtype",[Data.SQLDBType]::VarChar, 30))) | Out-Null
     $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@id",[Data.SQLDBType]::VarChar, 30))) | Out-Null
     $sqlCommand.Parameters[0].Value = $key
     $sqlCommand.Parameters[1].Value = $value
     try{
-        if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
+         $InsertedID = $sqlCommand.ExecuteScalar()
+    } catch {
+        write-host "Exception processing typeid="$key" id"=$value
+        $ErrorMessage = $_.Exception.Message
+        $hh=get-date -f MM/dd/yyyy_HH:mm:ss
+        write-host `n"URL"$url
+        write-host `n$hh   $ErrorMessage
+    }
+}
+
+function InsertPubXML ([Data.SqlClient.SqlConnection] $sqlConnection,$key,$value) {
+    if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
             $sqlConnection.Open()
-        }
+    }
+    $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
+    $sqlCommand.Connection = $sqlConnection
+    $sqlCommand.CommandText = "SET NOCOUNT ON; " +
+        "if not exists (select * from  [Symplectic.Elements].[PubXML] where idtype=@val1 and id=@val2)"+
+        " begin "+
+        " INSERT INTO [Symplectic.Elements].[PubXML] (XmlCol,idtype,id )"+
+        " SELECT CONVERT(XML, BulkColumn) AS BulkColumn,@idtype,@id "+ 
+        " FROM OPENROWSET(BULK 'D:\app\DesignSymplistic\data_utf8.txt', SINGLE_BLOB) AS x"+
+        " end;"
+    $sqlcommand.CommandTimeout=120
+    $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@val1",[Data.SQLDBType]::VarChar, 30))) | Out-Null
+    $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@val2",[Data.SQLDBType]::VarChar, 30))) | Out-Null
+    $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@idtype",[Data.SQLDBType]::VarChar, 30))) | Out-Null
+    $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@id",[Data.SQLDBType]::VarChar, 30))) | Out-Null
+    $sqlCommand.Parameters[0].Value = $key
+    $sqlCommand.Parameters[1].Value = $value
+    $sqlCommand.Parameters[2].Value = $key
+    $sqlCommand.Parameters[3].Value = $value
+    try{
         $InsertedID = $sqlCommand.ExecuteScalar()
     } catch {
         write-host "Exception processing typeid="$key" id"=$value
         $ErrorMessage = $_.Exception.Message
-        get-date -f MM/dd/yyyy_HH:mm:ss
+        $hh=get-date -f MM/dd/yyyy_HH:mm:ss
         write-host `n"URL"$url
-        write-host `n$ErrorMessage
+        write-host `n$hh  $ErrorMessage
     }
 }
 
+
 function SQLExecuteCommand ([Data.SqlClient.SqlConnection] $sqlConnection,$command ) {
- 
+    if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
+      $sqlConnection.Open()
+    }
     try {
         $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
         $sqlCommand.Connection = $sqlConnection
         $sqlCommand.CommandText = $command
-        if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
-            $sqlConnection.Open()
-        }
+        $sqlcommand.CommandTimeout=120
         $InsertedID = $sqlCommand.ExecuteScalar()
     } catch {
         write-host "Exception processing command"$command
         $ErrorMessage = $_.Exception.Message
-        get-date -f MM/dd/yyyy_HH:mm:ss
-        write-host `n$ErrorMessage
+        $hh=get-date -f MM/dd/yyyy_HH:mm:ss
+        write-host `n$hh   $ErrorMessage
     }
 }
  
 # Open SQL connection (you have to change these variables)
-$DBserver="stage-sql-ctsi.ucsf.edu"
-$DBName="profiles_ucsf"
-$dbuser="App_Profiles10"
-$dbpassword="Password1234"
-$wusername="ucsf"
-$wpassword="t3PLMpzX98J"
-$apiurl="https://qa-oapolicy.universityofcalifornia.edu:8002/elements-secure-api"
-#$apiurl="https://oapolicy.universityofcalifornia.edu:8002/elements-secure-api"
+$tableKeys=""
+$tableValues=""
+write-host "count=" $args.count "arguments=" $args
+$params=$args[0..($args.count-1)]
+write-host "params="$params
+
+# Take settings from config file
+if ($PWD) {$myDir=$PWD}
+if ($MyInvocation) {
+    $myDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+}
+[xml]$ConfigFile = Get-Content "$MyDir\Symplectic.xml"
+
+$DBserver=$ConfigFile.Settings.Database.DBServer
+$DBName=$ConfigFile.Settings.Database.DBName 
+$dbuser=$ConfigFile.Settings.Database.DBUser 
+$dbpassword=$ConfigFile.Settings.Database.DBPassword 
+$wusername=$ConfigFile.Settings.WebService.WSUser
+$wpassword=$ConfigFile.Settings.WebService.WSPassword 
+$apiurl=$ConfigFile.Settings.WebService.URL 
+
+$i=0
+$users=""
+$userslist=@()
+$users4db=""
+$force=""
+$DEBUG=0
+#$params="-users", "000732@ucsf.edu,000983@ucsf.edu,001710@ucsf.edu"
+while ($i -lt $params.Length) {
+    if ($DEBUG -eq 1) {
+        write-host "i="$i
+        write-host "current arg=" $params[$i] "==" $params[$i+1]
+    }
+    if ($params[$i] -eq "-users") {
+        $users=$params[$i+1]
+        if ($users -ne "all"){
+            $userslist= $users  -split ','
+            $j=0
+            while ($j -lt $userslist.Length) {
+                if ($j -eq $userslist.Length-1) {
+                    $users4db=$users4db+"'"+$userslist[$j]+"'"
+                } else {
+                    $users4db=$users4db+"'"+$userslist[$j]+"',"
+                }
+                $j++
+            }
+            $users4db="("+$users4db+")"
+            $DEBUG=1
+        }
+        if ($DEBUG -eq 1) {
+            write-host "users="$users
+            write-host "userslist="$userslist
+            write-host "users4db="$users4db
+        }
+        $i=$i+2
+        continue
+    }
+    if ($params[$i] -eq "-force") {
+        $force="true"
+    }
+    if ($params[$i] -eq "-debug"){
+        $DEBUG=1
+    } 
+    $i++
+}
+
 $allpubids=@()
 
 
@@ -197,8 +301,28 @@ if ($sqlConnection.State -ne [Data.ConnectionState]::Open) {
     Exit
 }
 
-$cmd="truncate table [profiles_ucsf].[Symplectic.Elements].[AllXML];truncate table [profiles_ucsf].[Symplectic.Elements].[Publication]"
-SQLExecuteCommand  $sqlConnection $cmd
+$cmd=""
+if ($DEBUG -eq 1) {
+    write-host "DB preparation"
+    write-host "users="$users
+    write-host "force="$force
+}
+if ($users -eq "all" -and $force -ne "") {
+    if ($DEBUG -eq 1) {write-host "Truncating DB tables"}
+    $cmd="truncate table [Symplectic.Elements].[UserXML];" +
+         "truncate table [Symplectic.Elements].[PubXML];"+
+         "truncate table [Symplectic.Elements].[Publication];"
+}
+if ($users4db -ne "") {
+    if ($DEBUG -eq 1) {write-host "Deleting existing users"}
+    $cmd="delete [Symplectic.Elements].[UserXML] where idtype in "+$users4db+
+         " or (idtype="+"'"+"UserName"+"'"+" and id in "+$users4db+
+         " ); delete [Symplectic.Elements].[Publication] where username in "+$users4db+";"
+}
+
+if ($cmd -ne ""){
+    SQLExecuteCommand  $sqlConnection $cmd
+}
 
 $userNames=@()
 $personids=@()
@@ -228,21 +352,23 @@ foreach ($userName in $userNames)
 {
     $url=$apiurl+"/users?username="+$userName
     [string] $atem=ReadXML $url $userName
-    if (-not $atem){ continue} #.Length -eq 0) 
+    if (-not $atem -or $atem.Length -eq 0){ continue} #.Length -eq 0) 
     [xml]$doc =[xml] $atem
     $authorid=$doc.feed.entry.object.id
     if (-not $authorid){ continue}
-    InsertXML $sqlConnection "UserName" $userName
+    InsertUserXML $sqlConnection "UserName" $userName
     $displayname=$doc.feed.entry.title
     #$personid=$pcolersonids[$i]
-    #write-host "author="$authorid ($displayname) "userName="$userName
+    if ($DEBUG -eq 1) {write-host "author="$authorid ($displayname) "userName="$userName}
     $url=$apiurl+"/users/"+$authorid+"/publications"
     [string] $atem=ReadXML $url
     if (-not $atem){continue}
     [xml]$doc=[xml] $atem
-    InsertXML $sqlConnection "lib_authorid" $authorid    
+    InsertUserXML $sqlConnection $userName $authorid    
 #cmd /c pause | out-null
     $pubs=GetUserPublications $doc
+    if ($DEBUG -eq 1) {write-host "for "$username" got pubs="$pubs}
+    if (-not $pubs -or $pubs.Lenght -eq 0 -or $pubs -eq "Exception") { continue}
     foreach ($lib_pubid in $pubs){
         $url=$apiurl+"/publications/"+$lib_pubid #1336238
 	[string] $atem=ReadXML $url
@@ -257,7 +383,7 @@ foreach ($userName in $userNames)
             continue
         }
         if ($savedpub -eq 0){
-            InsertXML $sqlConnection "lib_pubid" $lib_pubid
+            InsertPubXML $sqlConnection "lib_pubid" $lib_pubid
             $allpubids=$allpubids+($lib_pubid  -join ",")
         }
         #SQLExecuteCommand  $sqlConnection $cmd

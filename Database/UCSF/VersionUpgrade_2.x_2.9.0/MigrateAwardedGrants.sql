93c003d4af1bfe111b8a029fbd643359dd28e7c0
Eric Meeks <eric.meeks@ucsf.edu>
2016-10-27 10:08:52 -0700

    DB Script naming cleanup

---
+++ Database/UCSF/VersionUpgrade_2.x_2.9.0/MigrateAwardedGrants.sql
@@ -0,0 +1,58 @@
+
+begin tran 
+
+
+INSERT INTO [Profile.Data].[Funding.Agreement]
+           ([FundingAgreementID]
+           ,[FundingID]
+           ,[AgreementLabel]
+           ,[GrantAwardedBy]
+           ,[StartDate]
+           ,[EndDate]
+           ,[PrincipalInvestigatorName]
+           ,[Abstract]
+           ,[Source]
+           ,[FundingID2])
+     
+(
+select
+agp.grantprincipalpk as FundingAgreementID --,agpi.principalinvestigatorpk,ag.grantpk 
+,ag.fullprojectnum FundingID, ag.ProjectTitle as AgreementLabel,'NIH' as GrantAwardBy, convert(varchar(10),ProjectStart, 101) as StartDate
+,convert(varchar(10),ProjectEnd, 101) as StartDate
+,agpi.name PrinciaplInvestigatorName
+,'' as Abstract
+,'NIH' as Source
+,ag.coreprojectnumber as FundingID2
+--,* 
+from [ORNG.Grant].[agGrant]  ag 
+inner join [ORNG.Grant].[agGrantPrincipal] agp on ag.grantpk=agp.grantpk
+inner join [ORNG.Grant].[agPrincipalInvestigator] agpi on agpi.principalinvestigatorpk=agp.principalinvestigatorpk
+where not exists ( select * from [Profilesrns_29].[Profile.Data].[Funding.Agreement] rnsfa where rnsfa.fundingagreementid=agp.grantprincipalpk)
+--and ag.grantpk in ( select grantpk from  [ORNG.Grant].[agGrantPrincipal] where principalinvestigatorpk in ( select principalinvestigatorpk from [ORNG.Grant].[agPrincipalInvestigator] where name like '%cabana%'))
+)
+
+
+GO
+
+
+INSERT INTO [Profile.Data].[Funding.Role]
+           ([FundingRoleID]
+           ,[PersonID]
+           ,[FundingAgreementID]
+           ,[RoleLabel]
+           ,[RoleDescription])
+     
+	 (
+	 select  newid() as FundingRoleID  --agpi.principalinvestigatorpk
+, cast(agpi.employeeid as int) as Perosnid, agp.grantprincipalpk as Fundingagreementid,'Principal Investigator' as rolelabel, '' as roledescription
+from [ORNG.Grant].[agGrant]  ag 
+inner join [ORNG.Grant].[agGrantPrincipal] agp on ag.grantpk=agp.grantpk
+inner join [ORNG.Grant].[agPrincipalInvestigator] agpi on agpi.principalinvestigatorpk=agp.principalinvestigatorpk
+where
+not exists( select * from [Profile.Data].[Funding.Role] rnsfr where rnsfr.personid=cast(agpi.employeeid as int) and rnsfr.fundingagreementid=agp.grantprincipalpk) 
+--and ag.grantpk in ( select grantpk from  [ORNG.Grant].[agGrantPrincipal] where principalinvestigatorpk in ( select principalinvestigatorpk from [ORNG.Grant].[agPrincipalInvestigator] where name like '%cabana%'))
+)
+
+
+
+commit

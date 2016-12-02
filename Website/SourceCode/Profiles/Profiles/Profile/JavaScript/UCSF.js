e4dcd6d452c33fe28cc23b573e237ff1a314679b
cpiontko <cynthia.piontkowski@ucsf.edu>
2016-12-02 14:25:20 -0800

    Scrollable table style & extra line fix
    Extra line that appears when there are no external co-authors

--- Website/SourceCode/Profiles/Profiles/Profile/JavaScript/UCSF.js
+++ Website/SourceCode/Profiles/Profiles/Profile/JavaScript/UCSF.js
@@ -59,7 +59,14 @@
 			location.href = "#narrative";
 		});
     }
-	
+
+    //Remove border when no External Co-Authors
+    if ($('.panelPassive .gadgets-gadget-network-parent') && $('.panelPassive .gadgets-gadget-network-parent').length) {
+	if ($('.panelPassive .gadgets-gadget-network-parent').height() < 20) {
+		$('.panelPassive .gadgets-gadget-network-parent').css("cssText", "border: none !important;");
+	}
+    }
+		
 });
 
 

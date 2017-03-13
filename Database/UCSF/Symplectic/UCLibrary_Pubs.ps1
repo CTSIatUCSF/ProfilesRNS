37892ef85bfd489752fbd00767b734474b1ee26b
mgruzman <moisey.gruzman@ucsf.edu>
2017-03-13 12:29:20 -0700

    updated CDL reading script to get only new persons publications

--- Database/UCSF/Symplectic/UCLibrary_Pubs.ps1
+++ Database/UCSF/Symplectic/UCLibrary_Pubs.ps1
@@ -74,24 +74,65 @@
     SQLExecuteCommand  $sqlConnection $cmd
 }
 
-function GetUserPublications($doc){
+function GetUserPublications($url){
+  try {
     $pubs=@()
-    $i=0
-    try {
+   	$pageurl=$url
+   	[int]$page=1
+    if ($DEBUG -eq 1){write-host "connect to "$pageurl}
+    [string] $atem=ReadXML $pageurl
+    #if (-not $atem) 
+    [xml]$doc=[xml] $atem
+    InsertUserXML $sqlConnection $userName $authorid $page
+	$data=@()
+	$data=$doc.feed.pagination.ChildNodes
+	$data
+    write-host "++each in data++"
+    [int]$this=1
+    [int]$next=1
+    [int]$last=1
+    foreach ($element in $data) {
+			if ($DEBUG -eq 1) {write-host $element.position "=" $element.number}
+			if ($element.position -eq "this"){$this=[int]$element.number}
+			if ($element.position -eq "next"){$next=[int]$element.number}
+			if ($element.position -eq "last"){$last=[int]$element.number}
+	}
+	while ($page -le $last){
+	  if ($DEBUG -le 1){write-host "page=" $page "next" $next "last" $last}
+	  $i=0
       while ($true){
-        $lib_pubid=$doc.feed.entry[$i].relationship.related.object.id
-        if ("$lib_pubid" -eq ""){break}
-        if ($DEBUG -eq 1) {    write-host $lib_pubid}
-        $pubs=$pubs+($lib_pubid  -join ",")
-        $i++
+         write-host "i=" $i
+         $doc.feed.entry[$i]
+         $lib_pubid=$doc.feed.entry[$i].relationship.related.object.id
+         write-host "lib_pubid=" $lib_pubid
+         if ("$lib_pubid" -eq ""){break}
+         if ($DEBUG -eq 1) {    write-host $lib_pubid}
+         $pubs=$pubs+($lib_pubid  -join ",")
+         if ($DEBUG -eq 1) {write-host "added new pub " $lib_pubid "for "$username" got pubs="$pubs}
+         $i++
       }
-    } catch {
-        $ErrorMessage = $_.Exception.Message
-        $hh=get-date -f MM/dd/yyyy_HH:mm:ss
-        write-host `n$hh   $ErrorMessage
-	    $pubs="Exception"
-    }
-    return $pubs
+      $btem=""
+      if ($page -lt $next) {
+			$page=$next
+      }else{
+		    $page++
+		    if ($page -gt $last) {break}
+      }
+      $pageurl=$url+"?page="+[string]$page	
+	  if ($DEBUG -eq 1){write-host "connecting to "$pageurl}
+      [string] $btem=ReadXML $pageurl
+      [xml]$doc=[xml] $btem
+      InsertUserXML $sqlConnection $userName $authorid $page
+#cmd /c pause | out-null  
+    }         
+  } catch {
+      $ErrorMessage = $_.Exception.Message
+      $hh=get-date -f MM/dd/yyyy_HH:mm:ss
+      write-host `n$hh   $ErrorMessage
+      $pubs="0"
+  }
+  if ($DEBUG -eq 1){write-host "GetUserPublications returning pubs=" $pubs}
+  return $pubs
 }
 function ReadXML($url){
     $xmlstr=""
@@ -105,14 +146,15 @@ function ReadXML($url){
     	$start=$xmlstr.IndexOf(">")+1
     	$xmlstr=$xmlstr.Substring($start)
     	$xmlstr > 'D:\app\DesignSymplistic\data_utf8.txt'
-    	return $xmlstr
+    	write-host "we actually read xmlstr"
+     	return $xmlstr
     } catch {
         $ErrorMessage = $_.Exception.Message
         $hh=get-date -f MM/dd/yyyy_HH:mm:ss
         write-host `n"URL"$url
         write-host `n$hh   $ErrorMessage
+        return ""
     }
-    return $result
 }
 function ReadUserNames ([Data.SqlClient.SqlConnection] $sqlConnection) {
     if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
@@ -122,7 +164,9 @@ function ReadUserNames ([Data.SqlClient.SqlConnection] $sqlConnection) {
     $sqlCommand.Connection = $sqlConnection
     $sqlCommand.CommandText = "SET NOCOUNT ON; " +
         "Select username,personid from [User.Account].[User] "+
-	"where isActive=1 and personid is not NULL and username is not NULL"
+	"where isActive=1 and personid is not NULL and username is not NULL "+
+		"and username not in ( SELECT idtype FROM [Symplectic.Elements].[userXML] "+
+			" where id not like '%@%' and [page]=1 )"
     if ($users4db -ne "") {
         $sqlCommand.CommandText=$sqlCommand.CommandText+" and username in "+$users4db+";"
     } else {
@@ -136,31 +180,34 @@ function ReadUserNames ([Data.SqlClient.SqlConnection] $sqlConnection) {
         $userNames=$userNames+($reader[“userName”]  -join ",")
         $personids=$personids+($reader["personid"] -join ",")
         $lines=$lines+1
-        if ($DEBUG -eq 1) {if ($lines -ge 100) {break}}      
+        #if ($DEBUG -eq 1) {if ($lines -ge 100) {break}}      
     }
-    if ($DEBUG -eq 1) {write-host "all 5 users" $usernames}
+    $userNames=$userNames+("nobody@ucsf.edu"  -join ",")
+    if ($DEBUG -eq 1) {write-host "number of users" $usernames.Count}
     return $userNames 
 }
 
-function InsertUserXML ([Data.SqlClient.SqlConnection] $sqlConnection,$key,$value) {
+function InsertUserXML ([Data.SqlClient.SqlConnection] $sqlConnection,$key1,$key2,$key3) {
     if ($sqlConnection.State -eq [Data.ConnectionState]::Close) {
         $sqlConnection.Open()
     }
     $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
     $sqlCommand.Connection = $sqlConnection
     $sqlCommand.CommandText = "SET NOCOUNT ON; " +
-        " INSERT INTO [Symplectic.Elements].[UserXML] (XmlCol,idtype,id )"+
-        " SELECT CONVERT(XML, BulkColumn) AS BulkColumn,@idtype,@id "+ 
+        " INSERT INTO [Symplectic.Elements].[UserXML] (XmlCol,idtype,id,page,createdDT,updatedDT)"+
+        " SELECT CONVERT(XML, BulkColumn) AS BulkColumn,@idtype,@id,@page,getdate(),getDate() "+ 
         " FROM OPENROWSET(BULK 'D:\app\DesignSymplistic\data_utf8.txt', SINGLE_BLOB) AS x;"
     $sqlcommand.CommandTimeout=120
     $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@idtype",[Data.SQLDBType]::VarChar, 30))) | Out-Null
     $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@id",[Data.SQLDBType]::VarChar, 30))) | Out-Null
-    $sqlCommand.Parameters[0].Value = $key
-    $sqlCommand.Parameters[1].Value = $value
+    $sqlCommand.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@page",[Data.SQLDBType]::integer))) | Out-Null
+    $sqlCommand.Parameters[0].Value = $key1
+    $sqlCommand.Parameters[1].Value = $key2
+    $sqlCommand.Parameters[2].Value = $key3
     try{
          $InsertedID = $sqlCommand.ExecuteScalar()
     } catch {
-        write-host "Exception processing typeid="$key" id"=$value
+        write-host "Exception processing typeid="$key1" id"=$key2 "page="$key3
         $ErrorMessage = $_.Exception.Message
         $hh=get-date -f MM/dd/yyyy_HH:mm:ss
         write-host `n"URL"$url
@@ -177,8 +224,8 @@ function InsertPubXML ([Data.SqlClient.SqlConnection] $sqlConnection,$key,$value
     $sqlCommand.CommandText = "SET NOCOUNT ON; " +
         "if not exists (select * from  [Symplectic.Elements].[PubXML] where idtype=@val1 and id=@val2)"+
         " begin "+
-        " INSERT INTO [Symplectic.Elements].[PubXML] (XmlCol,idtype,id )"+
-        " SELECT CONVERT(XML, BulkColumn) AS BulkColumn,@idtype,@id "+ 
+        " INSERT INTO [Symplectic.Elements].[PubXML] (XmlCol,idtype,id,createdDT,updatedDT )"+
+        " SELECT CONVERT(XML, BulkColumn) AS BulkColumn,@idtype,@id,getdate(),getdate() "+ 
         " FROM OPENROWSET(BULK 'D:\app\DesignSymplistic\data_utf8.txt', SINGLE_BLOB) AS x"+
         " end;"
     $sqlcommand.CommandTimeout=120
@@ -248,7 +295,7 @@ $userslist=@()
 $users4db=""
 $force=""
 $DEBUG=0
-#$params="-users", "000732@ucsf.edu,000983@ucsf.edu,001710@ucsf.edu"
+#$params="-users", "028953@ucsf.edu,gogo@ucsf.edu,000983@ucsf.edu,nobody@ucsf.edu"
 while ($i -lt $params.Length) {
     if ($DEBUG -eq 1) {
         write-host "i="$i
@@ -298,7 +345,7 @@ $sqlConnection.Open()
 # Quit if the SQL connection didn't open properly.
 if ($sqlConnection.State -ne [Data.ConnectionState]::Open) {
     "Connection to DB is not open."
-    Exit
+    #Exit
 }
 
 $cmd=""
@@ -324,6 +371,7 @@ if ($cmd -ne ""){
     SQLExecuteCommand  $sqlConnection $cmd
 }
 
+
 $userNames=@()
 $personids=@()
 
@@ -331,10 +379,11 @@ $sqlConnection = New-Object System.Data.SqlClient.SqlConnection
 $sqlConnection = new-object System.Data.SqlClient.SQLConnection("Data Source=$DBserver;Initial Catalog=$DBName;User ID=$dbuser;Password=$dbpassword");
 $sqlConnection.Open()
 $usernames=ReadUserNames $sqlConnection
-#$userNames=$return[0]
-#$personids=$return[1]
+write-host $usernames
+
 
 $sqlConnection.Close()
+#Exit
 $sqlConnection.Open()
  
 # Quit if the SQL connection didn't open properly.
@@ -352,26 +401,35 @@ foreach ($userName in $userNames)
 {
     $url=$apiurl+"/users?username="+$userName
     [string] $atem=ReadXML $url $userName
-    if (-not $atem -or $atem.Length -eq 0){ continue} #.Length -eq 0) 
+    if (-not $atem -or $atem.Trim().Length -eq 0){ continue} #.Length -eq 0) 
     [xml]$doc =[xml] $atem
     $authorid=$doc.feed.entry.object.id
-    if (-not $authorid){ continue}
-    InsertUserXML $sqlConnection "UserName" $userName
+    if (-not $authorid){
+		if ($DEBUG -eq 1) {write-host "not found username=" $username}
+		$foundid="0"
+		InsertUserXML $sqlConnection "0" $userName 0
+		continue
+	}
+    InsertUserXML $sqlConnection $authorid $userName 0
     $displayname=$doc.feed.entry.title
-    #$personid=$pcolersonids[$i]
-    if ($DEBUG -eq 1) {write-host "author="$authorid ($displayname) "userName="$userName}
+    if ($DEBUG -eq 1) {write-host "author="$authorid ($displayname)- "userName="$userName}
     $url=$apiurl+"/users/"+$authorid+"/publications"
-    [string] $atem=ReadXML $url
-    if (-not $atem){continue}
-    [xml]$doc=[xml] $atem
-    InsertUserXML $sqlConnection $userName $authorid    
-#cmd /c pause | out-null
-    $pubs=GetUserPublications $doc
-    if ($DEBUG -eq 1) {write-host "for "$username" got pubs="$pubs}
-    if (-not $pubs -or $pubs.Lenght -eq 0 -or $pubs -eq "Exception") { continue}
-    foreach ($lib_pubid in $pubs){
+    $newpubs=@()
+    $newpubs=GetUserPublications $url 
+#cmd /c pause | out-null    
+    if ($DEBUG -eq 1) {write-host "for "$username" got newpubs="$newpubs}
+    #exit
+    if (-not $newpubs -or $newpubs.Lenght -eq 0 -or $newpubs -eq "Exception") { continue}
+#continue
+#in order to have only list of UCSF to compare with CDL usernames
+    foreach ($lib_pubid in $newpubs){
+		if($lib_pubid -match "^[0-9]*$" -or $lib_pubid -eq "" -or [string]::IsNullOrEmpty($lib_pubid) ){
+			write-host $lib_pubid "is int"
+		}else {
+			continue
+		}
         $url=$apiurl+"/publications/"+$lib_pubid #1336238
-	[string] $atem=ReadXML $url
+    	[string] $atem=ReadXML $url
     	if (-not $atem){continue} #
     	[xml]$doc=[xml] $atem
         $savedpub=0

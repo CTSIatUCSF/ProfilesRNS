dba716b658d9dda814d877bc93759cc16caba485
Eric Meeks <eric.meeks@ucsf.edu>
2017-03-16 14:52:48 -0700

    USC Template.Master panel

--- Website/SourceCode/Profiles/Profiles/Framework/Template.Master.designer.cs
+++ Website/SourceCode/Profiles/Profiles/Framework/Template.Master.designer.cs
@@ -76,6 +76,15 @@ namespace Profiles.Framework {
         protected global::System.Web.UI.WebControls.Panel MasterPageHeaderUCSD;
         
         /// <summary>
+        /// MasterPageHeaderUSC control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel MasterPageHeaderUSC;
+        
+        /// <summary>
         /// ContentActive control.
         /// </summary>
         /// <remarks>

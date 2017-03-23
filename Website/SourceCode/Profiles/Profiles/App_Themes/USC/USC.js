2da662380f09fe1b2e79194dbd17f244291449d8
cpiontko <cynthia.piontkowski@ucsf.edu>
2017-03-23 12:22:59 -0700

    USC About section
    .xml files & images in USC App_Theme, and Aoubt/FAQ.aspx

--- Website/SourceCode/Profiles/Profiles/App_Themes/USC/USC.js
+++ Website/SourceCode/Profiles/Profiles/App_Themes/USC/USC.js
@@ -1,30 +1,55 @@
 ﻿$(document).ready(function () {
 	
-    //alumni badge 
-	 if ($('.education') && $('.education').length) {
-		$(".education:contains('University of California, San Diego')").addClass('alum');
-		$(".education:contains('University of California San Diego')").addClass('alum');
-		$(".education:contains('University of California at San Diego')").addClass('alum');
-		$(".education:contains('University of California in San Diego')").addClass('alum');
-		$(".education:contains('UC, San Diego')").addClass('alum');
-		$(".education:contains('UC San Diego')").addClass('alum');
-		$(".education:contains('UCSD')").addClass('alum');
-		if ($('.alum') && $('.alum').length) $('.profilesContentMain').addClass('alumni');
-	 }
+	//Search Form: featured options
+    if ($('#trOtherOptions') && $('#trOtherOptions').length) {
+		$("#divOtherOptions label[for='ctcFirst_rMaster_ctl00_rDetail_ctl07_checkDetailText']").parent().addClass('trials1');
+		$('.trials2').click(function() { 
+			if ($('.trials2').is(':checked')) {
+				$('#ctcFirst_rMaster_ctl00_divDetail .trials1 :checkbox').attr("checked", true)[0].onclick();
+			} else {
+				$('#ctcFirst_rMaster_ctl00_divDetail .trials1 :checkbox').attr("checked", false)[0].onclick(); 
+			}
+		});
+		$("#divOtherOptions label[for='ctcFirst_rMaster_ctl00_rDetail_ctl00_checkDetailText']").parent().addClass('faculty1');
+		$('.faculty2').click(function() { 
+			if ($('.faculty2').is(':checked')) {
+				$('#ctcFirst_rMaster_ctl00_divDetail .faculty1 :checkbox').attr("checked", true)[0].onclick();
+			} else {
+				$('#ctcFirst_rMaster_ctl00_divDetail .faculty1 :checkbox').attr("checked", false)[0].onclick(); 
+			}
+		});
+	}
 	
-    // altmetrics, don't attempt to load till after 7 seconds, which is 7000 milliseconds
-    setTimeout(function () {
-      if ($('#publicationListAll') && $('#publicationListAll').length) {
-        $("#publicationListAll li a:contains('PubMed')").each(function () {
-            var pmid = $(this).attr('href').match(/(\d+)$/);
-            if (pmid && pmid[0]) {
-                $(this).parent().append(
-                " <span class='altmetric-embed' data-badge-popover='bottom' data-badge-type='4' data-hide-no-mentions='true' data-pmid='" +
-                pmid[0] + "'></span>")
-            }
-        });
-        $.getScript('http://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js');
-      }
-    }, 7000);
- 
+    // Back to top http://typicalwhiner.com/116/effortless-jquery-floating-back-to-top-script-v2/
+    var pxShow = 300; //height on which the button will show  
+    var fadeInTime = 1000;  //how slow/fast you want the button to show  
+    var fadeOutTime = 1000;  //how slow/fast you want the button to hide  
+    var scrollSpeed = 1000;  //how slow/fast you want the button to scroll to top
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= pxShow) {
+            $("#backtotop").fadeIn(fadeInTime);
+        } else {
+            $("#backtotop").fadeOut(fadeOutTime);
+        }
+    });
+
+    $('#backtotop a').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, scrollSpeed);
+        if (window.location.hash.length > 1) {
+            window.location.hash = "";
+        }
+        return false;
+    });
+	
+	// PlumX metrics, don’t attempt to load till after 7 seconds, which is 7000 milliseconds 
+	setTimeout(function () { 
+		$("#publicationListAll li a:contains('PubMed')").each(function () { 
+			var pmid = $(this).attr('href').match(/(\d+)$/); 
+			if (pmid && pmid[0]) { 
+				$(this).parent().append("<a href='https://plu.mx/usc/a/?pmid=" + pmid[0] + "' class='plumx-plum-print-popup' data-popup='bottom' data-hide-when-empty='true' data-site='usc' data-badge='true'></a>");
+			}
+		});
+		$.getScript('http://d39af2mgp1pqhg.cloudfront.net/widget-popup.js');
+	}, 7000);
+
 });

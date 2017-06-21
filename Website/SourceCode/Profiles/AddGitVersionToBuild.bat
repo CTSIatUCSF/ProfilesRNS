4dd67f35079830e94fa2aad6049c720ca981dc30
mgruzman <moisey.gruzman@ucsf.edu>
2017-06-21 12:04:43 -0700

    modified for any machine

--- Website/SourceCode/Profiles/AddGitVersionToBuild.bat
+++ Website/SourceCode/Profiles/AddGitVersionToBuild.bat
@@ -1,4 +1,4 @@
 set projectDir=%1
-"C:\Program Files\Git\bin\git.exe" rev-parse --abbrev-ref HEAD > %projectDir%GitVersion.txt
+"git.exe" rev-parse --abbrev-ref HEAD > %projectDir%GitVersion.txt
 echo. >>  %projectDir%GitVersion.txt
-"C:\Program Files\Git\bin\git.exe" describe --tags --long >>  %projectDir%GitVersion.txt
+"git.exe" describe --tags --long >>  %projectDir%GitVersion.txt

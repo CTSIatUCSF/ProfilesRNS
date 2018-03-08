8c0fcb3d57488f6d2c50866606505f545152c30e
Eric Meeks <eric.meeks@ucsf.edu>
2018-03-08 14:12:15 -0800

    RDF link bug fix

--- Website/SourceCode/Profiles/Profiles/Framework/Modules/MainMenu/MainMenu.ascx.cs
+++ Website/SourceCode/Profiles/Profiles/Framework/Modules/MainMenu/MainMenu.ascx.cs
@@ -108,13 +108,13 @@ namespace Profiles.Framework.Modules.MainMenu
                     string uri = this.BaseData.SelectSingleNode("//rdf:RDF/rdf:Description/@rdf:about", base.Namespaces).Value;
 
                     //IF the URI is in our system then we build the link. If not then we do not build the link for the data.
-                    if (uri.Contains(Brand.GetDomain()))
+                    if (uri.Contains(Root.Domain))
                     {
                         string file = string.Empty;
                         string spostring = string.Empty;
                         string[] spoarray;
 
-                        spostring = uri.ToLower().Replace(Brand.GetDomain().ToLower() + "/profile/", "");
+                        spostring = uri.ToLower().Replace(Root.Domain.ToLower() + "/profile/", "");
                         spoarray = spostring.Split('/');
 
                         for (int i = 0; i < spoarray.Length; i++)

170e37f7e3bac28fcb8c330cf3a9aee0cbde501f
Meeks <eric.meeks@ucsf.edu>
2018-10-25 14:22:19 -0700

    UCD to UC Davis change in footer

--- Website/SourceCode/Profiles/Profiles/Framework/Template.Master.designer.cs
+++ Website/SourceCode/Profiles/Profiles/Framework/Template.Master.designer.cs
@@ -490,15 +490,6 @@ namespace Profiles.Framework {
         protected global::System.Web.UI.WebControls.Repeater rptFooter;
         
         /// <summary>
-        /// BackToTop control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image BackToTop;
-        
-        /// <summary>
         /// MasterPageQeryzUCSF control.
         /// </summary>
         /// <remarks>

de1ab535c24e5bd573104d5de75c980a03e574af
Meeks <eric.meeks@ucsf.edu>
2020-11-13 09:15:45 -0800

    UCD Student Projects search

--- Website/SourceCode/Profiles/Profiles/App_Themes/UCD/UCD.js
+++ Website/SourceCode/Profiles/Profiles/App_Themes/UCD/UCD.js
@@ -1,4 +1,15 @@
 ﻿$(document).ready(function () {
-	
+
+    //Search Form: featured options
+    if ($('#selOtherOptions') && $('#selOtherOptions').length) {
+        $("#divMaster_CTC1 label:contains('Student Projects')").parent().addClass('studentProjects1');
+        $('.studentProjects2').click(function () {
+            if ($('.studentProjects2').is(':checked')) {
+                $('.studentProjects1 input:checkbox').attr("checked", true)[0].onclick();
+            } else {
+                $('.studentProjects1 input:checkbox').attr("checked", false)[0].onclick();
+            }
+        });
+    }
 
 });
